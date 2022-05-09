290ad31eb1831b7e0e91a3b39941eb6c6c9c4eca
myazarc <myazarc@yandex.com>
2022-05-09 22:50:27 +0300

    initial UserService Test for tdd

---
+++ __test__/UserService.test.ts
@@ -0,0 +1,10 @@
+import request from "supertest";
+import app from "../src/app";
+
+describe("UserServices /user", () => {
+  it("POST /register => created user", () => {});
+
+  it("POST /login => created token", () => {});
+
+  it("GET /confirmation/:usermailtoken => approved user mail", () => {});
+});
